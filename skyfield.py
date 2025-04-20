4da85fdbb9bf81efaaecad28e4869f2a7152b0da
njangid2 <126363570+njangid2@users.noreply.github.com>
2025-04-20 15:04:17 -0500

    Create skyfield.py

---
+++ skyfield.py
@@ -0,0 +1,22 @@
+from skyfield.api import load
+import numpy as np
+
+
+#This is the skyfield implementation
+ts = load.timescale()
+eph = load('de440s.bsp')
+earth = eph['Earth']
+sun = eph['Sun']
+moon = eph['Moon']
+ts = load.timescale()
+jd = 2460785.5000000
+earthp = earth.at(ts.ut1_jd(jd)).position.km
+sunp = sun.at(ts.ut1_jd(jd)).position.km
+moonp = moon.at(ts.ut1_jd(jd)).position.km
+
+
+print(earthp-moonp)
+print(sunp)
+#print(moonp)
+
+print(np.linalg.norm(earthp-moonp))
